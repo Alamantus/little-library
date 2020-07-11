144723a14638a13abeb07461c1e8ff72bbe3a5c2
Robbie Antenesse <dev@alamantus.com>
2020-07-11 12:50:00 -0600

    Emulate outbox pagination to mimic Mastodon's output

--- routes/activitypub/get_outbox.js
+++ routes/activitypub/get_outbox.js
@@ -5,6 +5,8 @@ const settings = require('../../settings.json');
 
 module.exports = function (app) {
   app.server.get('/activitypub/outbox', function (req, res) {
+    const isPage = typeof req.query.page !== 'undefined' && req.query.page === 'true';
+
     const addedBooks = [...app.shelfCache, ...app.historyCache].map(bookData => {
       return {
         title: bookData.title,
@@ -32,51 +34,64 @@ module.exports = function (app) {
       ...addedBooks,
       ...historyBooks,
     ].sort((a, b) => a.date - b.date);
+
+    const orderedItems = bookDetails.map(bookData => {
+      bookData.author = bookData.author ? bookData.author : '<em>author not provided</em>';
+      bookData.contributor = bookData.contributor ? bookData.contributor : 'Anonymous';
+      let content;
+      if (bookData.action === 'added') {
+        content = `<p>New ${bookData.fileType} added: ${bookData.title} by ${bookData.author}</p><p>When adding, ${bookData.contributor} commented:</p><p>${md(bookData.summary)}</p>`;
+      } else {
+        content = `<p>The ${bookData.fileType} file of ${bookData.title} by ${bookData.author} (originally added by ${bookData.contributor}) has been removed from the shelf.</p>`;
+      }
+      const published = fecha.format(new Date(bookData.date), 'isoDateTime');
+      return {
+        id: `https://${settings.domain}/activitypub/create-${bookData.date}`,
+        type: 'Create',
+        actor: `https://${settings.domain}/activitypub/actor`,
+        object: {
+          id: `https://${settings.domain}/activitypub/${bookData.date}`,
+          type: 'Note',
+          summary: null,
+          inReplyTo: null,
+          published,
+          url: `https://${settings.domain}/activitypub/${bookData.date}`,
+          attributedTo: `https://${settings.domain}/activitypub/actor`,
+          content,
+          sensitive: false,
+          to: [
+            'https://www.w3.org/ns/activitystreams#Public',
+          ],
+          cc: [
+            `https://${settings.domain}/activitypub/followers`,
+          ],
+          attachment: [],
+          tag: [],
+          replies: [],
+        },
+      };
+    });
     
-    const outbox = JSON.stringify({
+    const outbox = {
       '@context': 'https://www.w3.org/ns/activitystreams',
+      id: `https://${settings.domain}/activitypub/outbox`,
       summary: 'Activities on ' + settings.siteTitle,
       type: 'OrderedCollection',
-      totalItems: bookDetails.length,
-      orderedItems: bookDetails.map(bookData => {
-        bookData.author = bookData.author ? bookData.author : '<em>author not provided</em>';
-        bookData.contributor = bookData.contributor ? bookData.contributor : 'Anonymous';
-        let content;
-        if (bookData.action === 'added') {
-          content = `<p>New ${bookData.fileType} added: ${bookData.title} by ${bookData.author}</p><p>When adding, ${bookData.contributor} commented:</p><p>${md(bookData.summary)}</p>`;
-        } else {
-          content = `<p>The ${bookData.fileType} file of ${bookData.title} by ${bookData.author} (originally added by ${bookData.contributor}) has been removed from the shelf.</p>`;
-        }
-        const published = fecha.format(new Date(bookData.date), 'isoDateTime');
-        return {
-          id: `https://${settings.domain}/activitypub/create-${bookData.date}`,
-          type: 'Create',
-          actor: `https://${settings.domain}/activitypub/actor`,
-          object: {
-            id: `https://${settings.domain}/activitypub/${bookData.date}`,
-            type: 'Note',
-            summary: null,
-            inReplyTo: null,
-            published,
-            url: `https://${settings.domain}/activitypub/${bookData.date}`,
-            attributedTo: `https://${settings.domain}/activitypub/actor`,
-            content,
-            sensitive: false,
-            to: [
-              'https://www.w3.org/ns/activitystreams#Public',
-            ],
-            cc: [
-              `https://${settings.domain}/activitypub/followers`,
-            ],
-            attachment: [],
-            tag: [],
-            replies: [],
-          },
-        };
-      }),
-    });
+    };
+    if (!isPage) {
+      outbox.totalItems = bookDetails.length;
+      outbox.first = `https://${settings.domain}/activitypub/outbox?page=true`;
+      outbox.last = `https://${settings.domain}/activitypub/outbox?page=true`;
+    } else {
+      outbox.id += '?page=true';
+      outbox.type += 'Page';
+      outbox.next = `https://${settings.domain}/activitypub/outbox?page=true`;
+      outbox.prev = `https://${settings.domain}/activitypub/outbox?page=true`;
+      outbox.partOf = `https://${settings.domain}/activitypub/outbox`;
+      outbox.orderedItems = orderedItems;
+    }
 
     res.setHeader('Content-Type', 'application/activity+json');
-    res.send(outbox);
+    res.send(JSON.stringify(outbox));
   });
 }
